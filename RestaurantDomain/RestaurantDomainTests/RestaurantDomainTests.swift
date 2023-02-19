3e27cdf2c66e9d70ca3508ff0fbc6a43b980c88f
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2023-02-18 22:43:18 -0300

    removendo duplicidade de código nos testes, utilizando padrão factory para criação do SUT

--- RestaurantDomain/RestaurantDomainTests/RestaurantDomainTests.swift
+++ RestaurantDomain/RestaurantDomainTests/RestaurantDomainTests.swift
@@ -11,9 +11,7 @@ import XCTest
 final class RestaurantDomainTests: XCTestCase {
 
     func test_initializer_remoteRestaurantLoader_and_validate_url() async throws {
-        let url = try XCTUnwrap(URL(string: "https://google.com"))
-        let networkClient = NetworkClientSpy()
-        let sut = RemoteRestaurantLoader(url: url, networkClient: networkClient)
+        let (sut, networkClient, url) = try makeSUT()
         
         _ = await sut.load()
         
@@ -21,9 +19,7 @@ final class RestaurantDomainTests: XCTestCase {
     }
     
     func test_load_twice() async throws {
-        let url = try XCTUnwrap(URL(string: "https://google.com"))
-        let networkClient = NetworkClientSpy()
-        let sut = RemoteRestaurantLoader(url: url, networkClient: networkClient)
+        let (sut, networkClient, url) = try makeSUT()
         
         _ = await sut.load()
         _ = await sut.load()
@@ -32,9 +28,7 @@ final class RestaurantDomainTests: XCTestCase {
     }
     
     func test_load_and_returned_error_for_connectivity() async throws {
-        let url = try XCTUnwrap(URL(string: "https://google.com"))
-        let networkClient = NetworkClientSpy()
-        let sut = RemoteRestaurantLoader(url: url, networkClient: networkClient)
+        let (sut, networkClient, _) = try makeSUT()
         networkClient.state = .error(NSError(domain: "any error", code: -1))
         
         let returnedResult: RemoteRestaurantLoader.Error = await sut.load()
@@ -43,14 +37,20 @@ final class RestaurantDomainTests: XCTestCase {
     }
     
     func test_load_and_returned_error_for_invalidData() async throws {
-        let url = try XCTUnwrap(URL(string: "https://google.com"))
-        let networkClient = NetworkClientSpy()
-        let sut = RemoteRestaurantLoader(url: url, networkClient: networkClient)
+        let (sut, networkClient, _) = try makeSUT()
         networkClient.state = .success
         
         let returnedResult: RemoteRestaurantLoader.Error = await sut.load()
         
         XCTAssertEqual(returnedResult, .invalidData)
+    }
+    
+    private func makeSUT() throws -> (sut: RemoteRestaurantLoader, networkClient: NetworkClientSpy, url: URL) {
+        let url = try XCTUnwrap(URL(string: "https://google.com"))
+        let networkClient = NetworkClientSpy()
+        let sut = RemoteRestaurantLoader(url: url, networkClient: networkClient)
+        
+        return (sut, networkClient, url)
     }
 
 }
